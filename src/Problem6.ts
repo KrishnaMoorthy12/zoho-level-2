12bf0a323c989b3b552d0379c367564ccdb60642
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-02-10 21:40:57 +0530

    completed problem 6
    this problem was actually given in level 6

--- src/Problem6.ts
+++ src/Problem6.ts
@@ -4,13 +4,27 @@ export class Problem6 {
   constructor(private readonly scanner = new Scanner()) {}
 
   async main() {
-    const moneyInHouses = (await this.scanner.getInput(
-      'Enter the money in each house (space separated): '
-    )).split(' ').map(n=> +n);
+    const moneyInHouses = (
+      await this.scanner.getInput('Enter the money in each house (space separated): ')
+    )
+      .split(' ')
+      .map(n => +n);
 
     let sumOfOdds = 0;
     let sumOfEvens = 0;
 
-    for (let i = 0, j = 1; i < moneyInHouses.length && j ; )
+    for (let i = 0, j = 1; i < moneyInHouses.length && j < moneyInHouses.length; i += 2, j += 2) {
+      // since 0 corresponds to door #1 actually
+      sumOfOdds += moneyInHouses[i];
+      sumOfEvens += moneyInHouses[j];
+    }
+
+    if (sumOfEvens > sumOfOdds) {
+      console.log('Rob all the doors with even numbers!');
+    } else if (sumOfEvens < sumOfOdds) {
+      console.log('Rob all the doors with odd numbers!');
+    } else {
+      console.log('Whatever!!');
+    }
   }
 }
